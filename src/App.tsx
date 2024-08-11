43a5a08396d4e4910ab603cc195b9ffb3168b6e6
shahryarbhm <shahryarbahmeie@gmail.com>
2024-08-11 10:39:04 +0200

    feat: create simple react app for test functionality

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-use-before-define */
+/* eslint-disable no-console */
+
+"use client";
+
+function App() {
+
+    return (
+        <div className="container">
+            Hello World
+        </div>
+    );
+}
+
+export default App;
---
+++ src/Home.tsx
@@ -0,0 +1,12 @@
+
+import React from "react";
+import App from "./App";
+
+const Home: React.FC = () => {
+    return (
+        <App />
+    );
+};
+
+export default Home;
+
