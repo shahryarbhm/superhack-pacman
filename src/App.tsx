ce26fc4a385cebc32f1b4adb3a587cb6f99258ff
shahryarbhm <shahryarbahmeie@gmail.com>
2024-08-11 10:43:59 +0200

    feat: implement simple web3 auth login and logout functionality

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,105 @@
 
 "use client";
 
+import RPC from "./rpc/web3RPC";
+import { CHAIN_NAMESPACES, IProvider, WEB3AUTH_NETWORK } from "@web3auth/base";
+import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider";
+import { Web3Auth } from "@web3auth/modal";
+import { useEffect, useState } from "react";
+
+// import RPC from "./viemRPC";
+// import RPC from "./web3RPC";
+
+const clientId = "BPi5PB_UiIZ-cPz1GtV5i1I2iOSOHuimiXBI0e-Oe_u6X3oVAbCiAZOTEBtTXw4tsluTITPqA8zMsfxIKMjiqNQ"; // get from https://dashboard.web3auth.io
+
+const chainConfig = {
+    chainNamespace: CHAIN_NAMESPACES.EIP155,
+    chainId: "0xaa36a7",
+    rpcTarget: "https://rpc.ankr.com/eth_sepolia",
+    // Avoid using public rpcTarget in production.
+    // Use services like Infura, Quicknode etc
+    displayName: "Ethereum Sepolia Testnet",
+    blockExplorerUrl: "https://sepolia.etherscan.io",
+    ticker: "ETH",
+    tickerName: "Ethereum",
+    logo: "https://cryptologos.cc/logos/ethereum-eth-logo.png",
+};
+
+const privateKeyProvider = new EthereumPrivateKeyProvider({
+    config: { chainConfig },
+});
+
+const web3auth = new Web3Auth({
+    clientId,
+    web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_MAINNET,
+    privateKeyProvider,
+});
+
 function App() {
+    const [provider, setProvider] = useState<IProvider | null>(null);
+    const [loggedIn, setLoggedIn] = useState(false);
+    useEffect(() => {
+        const init = async () => {
+            try {
+                await web3auth.initModal();
+                setProvider(web3auth.provider);
+
+                if (web3auth.connected) {
+                    setLoggedIn(true);
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        init();
+    }, []);
+
+    const login = async () => {
+        const web3authProvider = await web3auth.connect();
+        setProvider(web3authProvider);
+        if (web3auth.connected) {
+            setLoggedIn(true);
+        }
+    };
+    const logout = async () => {
+        await web3auth.logout();
+        setProvider(null);
+        setLoggedIn(false);
+    };
+    function uiConsole(...args: any[]): void {
+        const el = document.querySelector("#console>p");
+        if (el) {
+            el.innerHTML = JSON.stringify(args || {}, null, 2);
+            console.log(...args);
+        }
+    }
+    const loggedInView = (
+        <>
+            <div className="flex-container">
+                <div>
+                    <button onClick={logout} className="card">
+                        Log Out
+                    </button>
+                </div>
+                <div>
+                </div>
+            </div>
+        </>
+    );
+
+    const unloggedInView = (
+        <button onClick={login} className="card">
+            Login
+        </button>
+    );
 
     return (
         <div className="container">
-            Hello World
+            <div className="grid">{loggedIn ? loggedInView : unloggedInView}</div>
+            <div id="console" style={{ whiteSpace: "pre-line" }}>
+                <p style={{ whiteSpace: "pre-line" }}></p>
+            </div>
         </div>
     );
 }
